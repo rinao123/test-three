8d5e51afe82f7a7b166b7a40e729faa9360e24b8
rinao <180233999@qq.com>
2021-01-31 17:40:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Park3D />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Park3D from "./components/Park3D.vue";
+
+@Options({
+  components: {
+    Park3D
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
